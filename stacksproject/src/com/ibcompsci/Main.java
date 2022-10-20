59aa49d3a6bd7ecb50180a59ae6869f90b8fc01b
JayZeeXx <rahulremany@gmail.com>
2022-10-19 23:36:05 -0700

    implemented algorithm that works

--- stacksproject/src/com/ibcompsci/Main.java
+++ stacksproject/src/com/ibcompsci/Main.java
@@ -1,36 +1,67 @@
 package com.ibcompsci;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    static Scanner scan = new Scanner(System.in);
-    static int[] queueFeeder;
-    static Queue queue = new LinkedList();
     public static void main(String[] args) {
-        startProgram();
+        getPrimes(20);
     }
-    public static void startProgram(){
-        System.out.println("How many numbers would you like to validate?");
-        int numbs = scan.nextInt();
-        while(numbs<1){
-            System.out.println("Try again");
-            numbs = scan.nextInt();
-        }
-        queueFeeder = new int[numbs];
-        for(int i = 0; i<queueFeeder.length;i++){
-            System.out.println("input next number");
-            queueFeeder[i] = scan.nextInt();
-        }
-        for(int i = 0; i<queueFeeder.length;i++){
-            queue.add(queueFeeder[i]);
-        }
-        runPrime(queue);
 
+    public static Queue<Integer> getPrimes(int num) {
+        Queue<Integer> numbers = new LinkedList<>();
+        Queue<Integer> result = new LinkedList<>();
+
+        for (int i = 2; i <= num; i++) {
+            numbers.add(i);
+        }
+
+        int n = 0;
+
+        do {
+            if (checkForPrime(numbers.peek())) {
+                n = numbers.remove();
+                result.add(n);
+            }
+
+            ArrayList<Integer> temp = new ArrayList<>(numbers);
+
+            for (int i = 0; i < temp.size(); i++) {
+                if (temp.get(i) % n == 0) {
+                    temp.remove(i);
+                }
+            }
+
+            numbers = new LinkedList<>(temp);
+
+        } while (n < Math.sqrt(num));
+
+        while (!numbers.isEmpty()) {
+            result.add(numbers.remove());
+        }
+
+        System.out.println(result);
+
+        return result;
     }
-    public static void runPrime(Queue queue){
 
+    public static boolean checkForPrime(int inputNumber) {
+        boolean isItPrime = true;
+
+        if (inputNumber <= 1) {
+            isItPrime = false;
+
+            return isItPrime;
+        } else {
+            for (int i = 2; i<= inputNumber/2; i++) {
+                if ((inputNumber % i) == 0) {
+                    isItPrime = false;
+
+                    break;
+                }
+            }
+
+            return isItPrime;
+        }
     }
 }
+
